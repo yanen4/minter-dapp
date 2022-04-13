80d785c9d868b1641145c73636d50e95ee833500
yanen4 <yanenjustin40@gmail.com>
2022-04-13 19:29:05 +0200

    SUp

--- backend/src/config.js
+++ backend/src/config.js
@@ -62,7 +62,7 @@ const TOKENS_PER_MINT = 3; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-04-18T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x25bE786824bE248112a46e29286B6046e854fF9b"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
